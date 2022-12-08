2bba6748ba6abeacc560a2295d2c66be4ae7d560
Jeter Medina <42383763+JeterMedina@users.noreply.github.com>
2022-12-08 08:06:09 -0600

    Final

--- delete.php
+++ delete.php
@@ -3,10 +3,11 @@
     require("./cors.php");
 
     $data = json_decode(file_get_contents('php://input'),true);
+    print_r($data);
     $id = $data['Num_Serie'];
     $conexion = new Conexion();
-    $db = conexion->getConexion();
-    $query= "DELETE FROM bass WHERE Num_Serie=:id";
+    $db = $conexion->getConexion();
+    $query= "DELETE FROM bajo WHERE Num_Serie=:id";
     $statement=$db->prepare($query);
     $statement->bindParam(':id',$id);
     $result = $statement->execute();
